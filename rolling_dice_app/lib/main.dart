6e533450a4d77d365048f7c3cd2d5c5acdebdf2f
Siddhant <sidcodeshere@gmail.com>
2024-01-26 23:08:56 +0530

    Nothing much on Day2

--- rolling_dice_app/lib/main.dart
+++ rolling_dice_app/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Container(
+    decoration:
+        const BoxDecoration(gradient: LinearGradient(colors: Colors.accents)),
+    child: const Center(
+      child: Text(
+        'This is the text meagend this is the message',
+        softWrap: true,
+        style: TextStyle(fontSize: 26, color: Colors.blue),
+      ),
+    ),
+  ))));
 }
-
